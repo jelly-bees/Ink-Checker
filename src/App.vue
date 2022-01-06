e37adb0602f41fffcab01444fb03d2f96012699c
Tim Noel <tnoel@eitccorp.com>
2022-01-06 15:17:11 -0500

    App vue app scaffolding

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <allergy-utility/>
+</template>
+
+<script>
+import AllergyUtility from './components/AllergyUtility.vue';
+
+export default {
+    name: 'App',
+    components: {
+        [AllergyUtility.name]: AllergyUtility
+    }
+};
+</script>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/AllergyUtility.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="allergy-container">
+        Insert app here lol
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'allergy-utility'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
